c8c084eb393033479195b4e2f896cc9e343814f9
shahram <shahramsamar2010@gmail.com>
2025-08-28 23:26:01 +0400

    Create database.py

---
+++ configurations/database.py
@@ -0,0 +1,20 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+
+uri = "mongodb+srv://shahram:gBKeebgu4JnCXtri@cluster0.0ovspnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db = client.todo_database    
+collection_name = db["todo_collection"]    
