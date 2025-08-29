edc7f392bb35ad8216f7931e244bd436c48782d4
shahram <shahramsamar2010@gmail.com>
2025-08-29 15:12:53 +0400

    use router

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
-
+from .routers.router import MyRouter
 
 app = FastAPI()
+app.include_router(MyRouter)
+
+# READ , UPDATE, POST, PUT, PATCH
 
 
-@app.get("/")
+@app.get("/test")
 async def root():
     return {"message": "Hello World", "mongodb_status": "connected"}
+
+@app.get("/name/{name}")
+async def say_my_name(name: str):
+    return [
+        {"message": f"Hello, my name is {name}!"},
+        {"message2": f"Hello, my name is {name}!"},
+        ]
